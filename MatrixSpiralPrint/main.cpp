d12ea2c44f0db5a33e22c1b33066ae606cd58e16
Nikolay Zemtsovskiy <zenikolas256@gmail.com>
2020-12-10 23:53:41 +0300

    fix print spiral rotation

--- MatrixSpiralPrint/main.cpp
+++ MatrixSpiralPrint/main.cpp
@@ -2,7 +2,8 @@
 // Given a 2D array, print it in spiral form
 #include <gtest/gtest.h>
 
-void printSpiral(int** arr, size_t N, size_t M) {
+template <size_t N, size_t M>
+void printSpiral(int (&arr)[N][M]) {
     bool visited[N][M];
     for (auto& line: visited) {
         for (auto& row: line) {
@@ -61,34 +62,25 @@ void printSpiral(int** arr, size_t N, size_t M) {
 TEST(PrintSpiralTeest, TwoXTwo) {
     const size_t N = 2;
     const size_t M = 2;
-    int** arr = new int*[N];
-    for (size_t i = 0; i < N; ++i) {
-        arr[i] = new int[M];
-    }
-
-    for (size_t i = 0; i < N; ++i) {
-        arr[i] = new int[M];
-    }
-    [N] = {{1,2}, {3,4}};
-    int* arrp = &arr[0][0];
-    printSpiral(&arrp, N, M);
+    int arr[N][N] = {{1,2}, {3,4}};
+    printSpiral(arr);
     //expected output: 1 2 4 3
 }
 
-//TEST(PrintSpiralTeest, FourXFour) {
-//    const size_t N = 4;
-//    const size_t M = 4;
-//    int arr[N][M] = {{1, 2, 3, 4}, {12, 13, 14, 5}, {11, 16, 15, 6}, {10, 9, 8, 7}};
-//
-//    printSpiral((int**)arr, N, M);
-//    //expected output: 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16
-//}
-//
-//TEST(PrintSpiralTeest, NotSquare) {
-//    const size_t N = 3;
-//    const size_t M = 6;
-//    int arr[N][M] = {{1, 2, 3, 4, 5, 6}, {7, 8, 9, 10, 11, 12}, {13, 14, 15, 16, 17, 18}};
-//
-//    printSpiral((int**)arr, N, M);
-//    //expected output: 1 2 3 4 5 6 12 18 17 16 15 14 13 7 8 9 10 11
-//}
+TEST(PrintSpiralTeest, FourXFour) {
+    const size_t N = 4;
+    const size_t M = 4;
+    int arr[N][M] = {{1, 2, 3, 4}, {12, 13, 14, 5}, {11, 16, 15, 6}, {10, 9, 8, 7}};
+
+    printSpiral(arr);
+    //expected output: 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16
+}
+
+TEST(PrintSpiralTeest, NotSquare) {
+    const size_t N = 3;
+    const size_t M = 6;
+    int arr[N][M] = {{1, 2, 3, 4, 5, 6}, {7, 8, 9, 10, 11, 12}, {13, 14, 15, 16, 17, 18}};
+
+    printSpiral(arr);
+    //expected output: 1 2 3 4 5 6 12 18 17 16 15 14 13 7 8 9 10 11
+}
